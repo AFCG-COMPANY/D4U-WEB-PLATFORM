8094165b715423bcb13aa5675ca0df9837f73417
FedorArbuzovHSE <arbuzovfp@gmail.com>
2018-05-29 23:17:02 +0300

    fix

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const app = express()
 
+var port = process.env.PORT || 8080;
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(8080, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log('Our app is running on http://localhost:' + port))
