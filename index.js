e68ff77461d5bfecc05f5e3038338340c220058c
FedorArbuzovHSE <arbuzovfp@gmail.com>
2018-05-30 23:06:17 +0300

    fix

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-
 const express = require('express')
+const exphbs  = require('express-handlebars')
+
 const app = express()
 
-var port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+const port = process.env.PORT || 8080;
+
+
+app.get('/', (req, res) => res.render('home'))
+
 
 app.listen(port, () => console.log('Our app is running on http://localhost:' + port))
