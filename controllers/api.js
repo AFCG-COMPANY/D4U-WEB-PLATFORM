840ad1d2f7d96bb20a6b75fce4a868a78bd4880c
FedorArbuzovHSE <arbuzovfp@gmail.com>
2018-06-04 20:36:13 +0300

    fix

--- controllers/api.js
+++ controllers/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/api', function(req, res, next) {
-    res.render('home');
+    res.send('hello api');
 });
 
 module.exports = router;
