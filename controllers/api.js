bc2ef95f4f86435e584c6c88ecc94dbac97ae59c
Fedor <arbuzovFP@gmail.com>
2018-06-12 01:50:46 +0400

    fix

--- controllers/api.js
+++ controllers/api.js
@@ -1,14 +1,11 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-/* GET home page. */
-router.get('/api', function(req, res, next) {
-    res.send({'status': 'ok'});
-});
 
 router.post('/auth', function(req, res, next) {
-    console.log(req.body)
-    res.send({'status': 'ok'});
+    userResp = req.body
+    console.log(userResp)
+    res.send({'status': 'ok'})
 });
 
-module.exports = router;
+module.exports = router
