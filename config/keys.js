4a0aa360d2b8032b8ec64a46621dd14a519dd2e8
Fedor <arbuzovFP@gmail.com>
2018-06-12 03:12:55 +0400

    fix

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+'use strict'
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.mongo_db_url = 'mongodb://fedor.a.hope:MGTA2019@ds016098.mlab.com:16098/d4u'
--- controllers/api.js
+++ controllers/api.js
@@ -1,24 +1,30 @@
 
-var
+const
     express = require('express')
-
-var
     router = express.Router()
-
-const
     MongoClient = require('mongodb').MongoClient;
-
-const
-    mongo_db_url = require('../../config/keys').mongo_db_url;
+    mongo_db_url = require('../config/keys').mongo_db_url
 
 router.post('/auth', function(req, res, next) {
     userResp = req.body
     console.log(userResp)
-    if(userResp['type'] == 'Login'){
+    const query = {login: userResp['login'], password: userResp['password']}
+    console.log(query)
+    if(userResp['type'] === 'Login'){
         // find user in database, and if it is ok send cookie
-
+        MongoClient.connect(mongo_db_url, function (err, db) {
+            if (err) throw err;
+            const dbo = db.db("d4u");
+            dbo.collection("users").findOne(query, function(err, result) {
+                console.log(result)
+                if (result.length === 1){
+                    console.log(result['cookie'])
+                }
+                db.close();
+            });
+        });
     }
-    else if(userResp['type'] == 'Registration'){
+    else if(userResp['type'] === 'Registration'){
         // if there is no email in database yet, register user
     }
     res.send({'status': 'ok'})
