77fc2e113314d8ed94831eb6404622d179e3f255
Fedor <arbuzovFP@gmail.com>
2018-06-12 01:38:46 +0400

    fix

--- controllers/api.js
+++ controllers/api.js
@@ -11,12 +11,4 @@ router.post('/auth', function(req, res, next) {
     res.send({'status': 'ok'});
 });
 
-router.get('/login', function(req, res, next) {
-    res.send('hello api');
-});
-
-router.get('/registration', function(req, res, next) {
-    console.log(res)
-    res.send('hello api')
-});
 module.exports = router;
