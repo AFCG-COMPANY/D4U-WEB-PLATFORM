12df8d1bae80cbad595f8d969457356092c0c6ba
FedorArbuzovHSE <arbuzovfp@gmail.com>
2018-06-14 21:43:26 +0300

    fix

--- controllers/api.js
+++ controllers/api.js
@@ -10,19 +10,21 @@ const
 
 router.post('/auth', function(req, res, next) {
     userResp = req.body
-    console.log(userResp)
     if(userResp['type'] === 'Login'){
         // find user in database, and if it is ok send cookie
         MongoClient.connect(mongo_db_url, function (err, db) {
             if (err) throw err;
             const dbo = db.db("d4u");
             dbo.collection("users").findOne({login: userResp['login'], password: userResp['password'], confirmed: true}, function(err, result) {
+                console.log({login: userResp['login'], password: userResp['password'], confirmed: true})
+                console.log(result)
                 if (result !== null) {
-                    res.cookie('token', userToken, { maxAge: 900000, httpOnly: true })
-                    res.redirect('/')
+                    //res.cookie('token', result['token'], { maxAge: 900000, httpOnly: true })
+                    //res.redirect('/')
+                    res.send({'status': '200', 'token': result['token']})
                 }
                 else {
-                    res.send({'status': 'ok', 'message': 'такого нет'})
+                    res.send({'status': '400', 'message': 'такого нет'})
                 }
                 db.close()
             })
@@ -36,7 +38,7 @@ router.post('/auth', function(req, res, next) {
             dbo.collection("users").findOne({login: userResp['login']}, function(err, result) {
                 console.log(result)
                 if (result !== null){
-                    res.send({'status': 'ok', 'message': 'Такой емаил уже есть!!'})
+                    res.send({'status': '400', 'message': 'Такой емаил уже есть!!'})
                 }
                 else{
                     // генерируем токен для пользовотеля
@@ -57,7 +59,7 @@ router.post('/auth', function(req, res, next) {
                     dbo.collection("users").insertOne(userInfo, function(err, result) {
                         console.log(result)
                         if (err) throw err
-                        res.send({'status': 'ok', 'message': 'Мы вас зарегестрировали!!'})
+                        res.send({'status': '200', 'message': 'Мы вас зарегестрировали!!'})
                         db.close()
                     })
                 }
@@ -78,7 +80,7 @@ router.get('/confirm/:token', function (req, res) {
                 dbo.collection("users").updateOne({token: userToken, confirmed: false}, { $set: {confirmed: true}}, function(err, result) {
                     if (err) throw err
                 })
-                res.cookie('token', userToken, { maxAge: 900000, httpOnly: true })
+                res.cookie('token', userToken, { maxAge: 900000})
                 res.redirect('/')
             }
             else{
