fae5f01dea0df992f5b10f8354f0c90a099f27c2
Fedor <arbuzovFP@gmail.com>
2018-09-09 16:32:08 +0400

    npm build

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const
 const
     app = express()
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
 app.use(cookieParser())
-app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -18,6 +20,12 @@ const
 
 
 app.use('/api', apiRouter)
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 
 app.listen(port, () => console.log('Our app is running on http://localhost:' + port))
