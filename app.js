e7ecca9839e18bd9ee84e5be62e1a1cc80ad25d6
Fedor <arbuzovFP@gmail.com>
2018-09-09 16:18:50 +0400

    fix app.js

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const
     express = require('express')
-    exphbs  = require('express-handlebars')
     cookieParser = require('cookie-parser')
     bodyParser = require('body-parser')
 const
     app = express()
 
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
 app.use(cookieParser())
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json())
@@ -18,15 +14,9 @@ const
     port = process.env.PORT || 8080
 
 const
-    indexRouter = require('./controllers/index')
-    aboutRouter = require('./controllers/about')
-    usersRouter = require('./controllers/user')
     apiRouter = require('./controllers/api')
 
 
-app.use('/', indexRouter)
-app.use('/', aboutRouter)
-app.use('/', usersRouter)
 app.use('/api', apiRouter)
 
 
