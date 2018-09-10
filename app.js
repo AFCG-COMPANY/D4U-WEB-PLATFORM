ef56b54684956cc1cdf859005f69a7c50b319792
Fedor <arbuzovFP@gmail.com>
2018-09-10 16:38:46 +0400

    delete public and views

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const
     app = express()
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.use(cookieParser())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const
     port = process.env.PORT || 8080
@@ -24,8 +24,8 @@ app.use('/api', apiRouter)
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+    res.sendFile(path.join(__dirname+'/client/build/index.html'))
+})
 
 
 app.listen(port, () => console.log('Our app is running on http://localhost:' + port))
