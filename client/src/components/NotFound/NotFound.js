be83ccbbbc3722b1570d328ec7f3b6bd9d3ad26a
Fedor <arbuzovFP@gmail.com>
2018-09-12 17:49:01 +0400

    fix

--- client/src/components/NotFound/NotFound.js
+++ client/src/components/NotFound/NotFound.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import './NotFound.css'
 
+import MenuAppBar from '../utils/MenuAppBar/MenuAppBar'
+
 const NotFound = () =>
-    <div className="text-wrapper">
-        <div className="title" data-content="404">
-            404
-        </div>
+    <div>
+        <MenuAppBar/>
+        <div className="text-wrapper">
+            <div className="title" data-content="404">
+                404
+            </div>
 
-        <div className="subtitle">
-            Oops, the page you're looking for doesn't exist.
-        </div>
+            <div className="subtitle">
+                Ой, похоже что этой страницы не существует.
+            </div>
 
-        <div className="buttons">
-            <a className="button" href="http://www.ajerez.es">Go to homepage</a>
+            <div className="buttons">
+                <a className="button" href="/">Вернуться на главную</a>
+            </div>
         </div>
     </div>
 
